1d25d070bd873820ea40a39c8b7152a85f032833
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-10 14:42:49 +0100

    add base price calc

--- main.js
+++ main.js
@@ -13,11 +13,15 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 // calcolo tariffa generica al km
 // applico condizioni scontistica in base all'eta, limitando a due il numero dei decimali
 
-
+// user inputs
 const distance = prompt('inserire la distanza che si vuole percorrere in km [10, 20.22]');
 console.log(distance);
 
 const age = prompt('isnerire la propria eta [15, 45, 20]');
 console.log(age);
 
+// base price calc
+const basePrice = distance * 0.21;
 
+// initialize final price
+let finalPrice;
