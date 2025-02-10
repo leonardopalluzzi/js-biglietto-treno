3fcf34c719ac991245a5668f8c2371fcf7a9729d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-11 00:29:01 +0100

    fix code structure

--- bonus/main_bonus.js
+++ bonus/main_bonus.js
@@ -11,7 +11,6 @@ quanto manca:  Stabilire l'ora attuale e attraverso una operazione matematica st
 
 // odd/even input
 let userChoice = Number(prompt('scegliere 1 per pari e 2 per dispari'));
-console.log(userChoice);
 
 let matchChoice;
 let sum;
@@ -21,37 +20,41 @@ if (userChoice != 1 && userChoice != 2) {
     do {
         userChoice = Number(prompt('per favore, scegliere 1 per pari e 2 per dispari'));
     } while (userChoice != 1 && userChoice != 2);
-} else {
-    if (userChoice == 1) {
-        matchChoice = "pari";
-    } else if (userChoice == 2) {
-        matchChoice = "dispari";
-    }
-    console.log(matchChoice);
 }
+
+if (userChoice == 1) {
+    matchChoice = "pari";
+} else if (userChoice == 2) {
+    matchChoice = "dispari";
+}
+
+
+console.log(userChoice);
+console.log(matchChoice);
 
 // user number input
 let userNumber = Number(prompt("scegliere un numero tra 1 e 9 da giocare"));
 
 //pc number generator
 const pcNumber = Math.floor(Math.random() * 9) + 1;
-console.log(pcNumber);
 
 
 // user input value check and sum calc
-if (userNumber >= 1 && userNumber <= 9) {
-    // sum values and rest calc
-    console.log(userNumber);
-    sum = userNumber + pcNumber;
-    console.log(sum);
-    rest = sum % 2;
-    console.log(rest);
-} else {
+if (userNumber < 1 || userNumber > 9) {
+    // sum value
     do {
         userNumber = Number(prompt("per favore, scegliere un numero intero compreso tra 1 e 9"));
     } while (userNumber < 1 || userNumber > 9);
 }
 
+sum = userNumber + pcNumber;
+rest = sum % 2;
+
+
+console.log(userNumber);
+console.log(pcNumber);
+console.log(sum);
+console.log(rest);
 
 // winner calc
 if ((rest == 0 && matchChoice == "pari") || (rest == 1 && matchChoice == "dispari")) {
